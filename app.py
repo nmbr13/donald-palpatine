0ac4ed1de83fafb6c66e7120f19700dff6585daf
Alex Gregorie <gregoriedev@gmail.com>
2018-09-20 23:41:59 -0400

    Begin templating

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
-from flask import Flask
+from flask import Flask, flash, redirect, render_template, request, session, abort
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
-    return "Hello World!"
+@app.route("/<string:name>/")
+def hello(name):
+    return render_template(
+        'index.html',name=name)
+
+if __name__ == "__main__":
+    app.run()
